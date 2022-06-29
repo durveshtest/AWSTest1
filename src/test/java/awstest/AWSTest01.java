fcc9b2110d735b8d0c57df4b1404be97f18ffdd9
durveshtest <108353923+durveshtest@users.noreply.github.com>
2022-06-30 00:25:54 +0530

    Update AWSTest01.java

--- src/test/java/awstest/AWSTest01.java
+++ src/test/java/awstest/AWSTest01.java
@@ -21,6 +21,7 @@ public class AWSTest01 {
         options.addArguments("disable-gpu");
 	options.addArguments("window-size=1920,1080");
         driver = new ChromeDriver(options);
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
     }
